4095690c57607d5983158c423a216852441d7cfa
ваше_имя <shenon_metew@mail.ru>
2023-05-16 03:49:13 +0300

    добавила иконки в шапку

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
-import Logo from '../Logo/Logo';
-import './header.css';
+import { PlusCircleOutlined, ProfileOutlined} from "@ant-design/icons";
+import Logo from "../Logo/Logo";
+import "./header.css";
+import Search from "../Search/Search";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <Logo />
+      <Search />
+      <div className="header__wrapper">
+      <div className="header__icon">
+       <span> <PlusCircleOutlined  className="ant-icon"/></span>
+       </div>
+      <div className="header__icon">
+      <span> <ProfileOutlined className="ant-icon"/></span>
+        </div>
+        </div>
+    </div>
+    
+  );
+};
 
-  const Header =() =>{
-return(
-  <div className='header'>
-<Logo />
-
-
-  </div>
-
-)
-  }
-
-  export default Header;
+export default Header;
