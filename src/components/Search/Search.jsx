8350d3264785852dfcf5aad6cd51bf282b95af02
ваше_имя <shenon_metew@mail.ru>
2023-05-16 20:18:16 +0300

    добавила поиск и его стили

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,18 @@
 import "./search.css";
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Search = ({ setSearch }) => {
   return (
-    <input
-      placeholder="Поиск..."
-      onChange={(e) => setSearch(e.target.value)}
-      className="search__input"
-    />
+    <div className="container__search">
+      <SearchIcon className="search__icon" />
+
+      <input
+        placeholder="Поиск..."
+        onChange={(e) => setSearch(e.target.value)}
+        className="search__input"
+      />
+    </div>
   );
 };
 
