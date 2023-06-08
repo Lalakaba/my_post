924f5df400221fbf316a388ab32e071f9c9a2dfc
Lalakaba23 <Shenon_Metew@mail.ru>
2023-06-09 00:25:23 +0300

    профиль и страницу профиля

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import {Routes, Route} from 'react-router-dom'
 import React, { useCallback, useEffect, useState } from 'react';
-import Header from './components/Header/Header';
+
 import { api } from './api';
-import { useDebounce } from "./components/MyHooks/hook"
-import {filteredPosts} from './others/something'
+
 import { ContextData } from './components/someContext/Context';
 
 //Подключение страниц
@@ -14,7 +13,9 @@ import { NotFound } from './pages/NotFound';
 import {  RessetPass } from './components/Profile/ResetPass';
 import { ChangePass } from './components/Profile/ChangePass';
 import { Registration } from './pages/Registration';
-import { AboutUser } from './others/InfoUser';
+import { AboutPost, AboutUser } from './others/InfoUserPost';
+
+
 
 
 
@@ -28,66 +29,36 @@ function App() {
   const [search, setSearch] = useState(undefined);
   const [user, setUser] = useState({});
   const [visible, setVisible] = useState(false);
-  const [favorites, setFavorites] = useState([]);
-  const debounceValue = useDebounce(search);
+  const [openModal, setOpenModal] = useState('');
   const [postComment, setPostComment] = useState([]);
   const [authorized, setAuthorized] = useState(false);
+  const [postInform, setPostInform] = useState(AboutPost)
   const [userInfo, setUserInfo] = useState(AboutUser);
-
+  const [currentUser, setCurrentUser] = useState({});
+  const [preliminaryAvatar, setPreliminaryAvatar] = useState("")
 //функция снятия и удаление лайка без перезагрузки страницы
 
 const handlePostLike = useCallback(async (post, isLiked) => {
   const updatedPost = await api.changeLikePostStatus(post._id, isLiked);
-  setPosts(s => [...s.map(e => e._id === updatedPost?._id ? updatedPost : e)]);// функция работает, видно в сети, но не меняется цвет сердце никак и счетчик не изменяется
+  setPosts(s => [...s.map(e => e._id === updatedPost?._id ? updatedPost : e)])
 
 }, [])
 
 
-//поиск по query
-useEffect(() => {
-  if (debounceValue === undefined)  
-  return
-  api.searchPosts(debounceValue)
-    .then((data) => setPosts(filteredPosts(data)
-    ))
-    .catch((error) => console.error( error)
-            );
-}, [debounceValue]);
+
 
 //получаем инфу о пользователе и все посты
 useEffect(() => {
   Promise.all([api.getAllPosts(), api.getUserInfo()])
-      .then(([data, user]) => {
-          setPosts(data);
-          setUser(user);
-      })
-      .catch((error) =>
-          console. error( error)
-      );
-}, [ postComment, userInfo]);
+    .then(([data, userData]) => {
+      setPosts(data);
+      setUser(userData);
+    })
+    .catch((error) => console.error(error));
+}, [postInform, postComment, userInfo]);
 
 
 
-
-  // useEffect(() => {                                              
-  //   api.getUserInfo().then((user) => {
-  //       setUser(user);
-  //     })
-  //     .catch((error) =>
-  //               console.error(error)
-  //           );
-  // }, []);
-
- 
-  //   useEffect(() => { 
-  //   api.getAllPosts().then((data) => {
-  //     // const filtered = filteredPosts(data)
-  //     setPosts(data);
-  //     })
-  //     .catch((error) =>
-  //               console.error(error)
-  //           );
-  // }, []);
 
   
 
@@ -105,17 +76,22 @@ useEffect(() => {
     setPosts,
     posts : posts,
     search,
+    setSearch,
     user,
     setUser,
     visible, 
     setVisible,
     userInfo,
     setUserInfo,
-    favorites,
     postComment, 
     setPostComment,
     authorized, 
-    setAuthorized
+    setAuthorized,
+    currentUser, 
+    setCurrentUser,
+    openModal, setOpenModal,
+    preliminaryAvatar, setPreliminaryAvatar,
+    postInform, setPostInform,
  
   }
 
@@ -129,7 +105,7 @@ useEffect(() => {
     <ContextData.Provider value={postsValue}>
   
 
-    <Header setSearch={setSearch} />
+   
       <Routes>
       
       <Route path='/registration' element={<Registration/>}/> 
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,25 +1,38 @@
 
 
 import "./index.css"
-// import { ContextData } from "../components/someContext/Context";
 import Profile from "../components/Profile/Profile";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ContextData } from "../components/someContext/Context";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import Header from "../components/Header/Header";
+import Modal from "../components/Modal/Modal";
+import ChangeAvatar from "../components/Profile/ChangeAvatar";
+import { api } from "../api";
+import { Icon } from "@blueprintjs/core";
+import EditProfile from "../components/Profile/EditProfile"
 
 export const ProfilePage  = () => {
-    const { user, setUserInfo, posts, visible, setVisible, userInfo } = useContext(ContextData);
-    const { name, about, email, avatar } = userInfo;
-    
-    // const userProfile = user._id === userId;
-    const handler = () => setVisible(true);
-     const closeHandler = () => {
-       setVisible(false);
-       console.log("closed");}
+    const { user, setUserInfo, openModal, setOpenModal, preliminaryAvatar, setPreliminaryAvatar } = useContext(ContextData);
+  
+    const userProfile = user._id 
+   
+    useEffect (() => {
+        api.getUserInfoById(user._id)
+            .then((userData) => {
+                setUserInfo(userData);
+                setPreliminaryAvatar(userData.avatar);
+            })
+            .catch((error) =>
+            console.error(error)
+            );
+    }, [ user._id, setUserInfo]);
+      
 
 
     return (
         <div className='profilePage_container'>
+             <Header/>
 
         <div>
             <Link to="/blogpage">
@@ -29,6 +42,36 @@ export const ProfilePage  = () => {
             <div>
             <Profile/>
             </div>
+
+            <div className="editProfile">
+            {userProfile && (
+          <Icon icon ="edit" className="editProfileIcon"  onClick={() => setOpenModal('avatar')}/> )}
+
+             {openModal === 'avatar' && (
+                        <Modal state={openModal === 'avatar'} setState={setOpenModal}>
+                            <ChangeAvatar
+                                preliminaryAvatar={preliminaryAvatar}
+                                setUserInfo={setUserInfo}
+                                setPreliminaryAvatar={setPreliminaryAvatar}/>
+                        </Modal>)}
+
+</div>
+
+                <div className="editProfileUser">
+
+                {userProfile && (
+          <Icon icon ="edit" className="editProfileIconInfo"  onClick={() => setOpenModal('editProfileInfo')}/> )}
+           
+                {openModal === 'editProfileInfo' && (
+               <Modal state={openModal === 'editProfileInfo'} setState={setOpenModal}>
+                    <EditProfile
+                    setUserInfo= {setUserInfo} 
+                    setOpenModal= {setOpenModal} /> 
+               </Modal>
+                )
+            }
+                </div>
+
             <div className='profilePost'>
              
 
@@ -44,3 +87,4 @@ export const ProfilePage  = () => {
     }
   
 
+export default ProfilePage;
